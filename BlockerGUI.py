f0721971603ab571c0f2bcb5ed0723f638cad81b
hailongowo <long31102005@gmail.com>
2022-03-16 21:34:33 +0700

    toggle

--- BlockerGUI.py
+++ BlockerGUI.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from turtle import onclick
 
 r = tk.Tk()
 r.geometry("800x450")
 r.title('Website Blocker')
+
+is_on = True
 
 header = tk.Label (r, text="Website Blocker", font="Arial 30")
 header.pack(pady=(70, 40))
@@ -18,4 +19,19 @@ url.pack(pady=20)
 
 button = tk.Button(r, text='Block!', font='Arial 15', width=30)
 button.pack(pady=5)
+
+def switch():
+    global is_on
+    if is_on:
+        toggle.config(image = off)
+        is_on = False
+    else:
+        toggle.config(image = on)
+        is_on = True
+ 
+on = tk.PhotoImage(file = "on.png")
+off = tk.PhotoImage(file = "off.png")
+toggle = tk.Button(r, image = on, bd = 0, command = switch)
+toggle.pack(pady = 50)
+
 r.mainloop()
